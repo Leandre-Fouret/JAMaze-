6479ac9d2d4626543335db84aed896e462c25c41
unknown <antoine.orange@epitech.eu>
2025-04-05 15:04:29 +0200

    feat: added texturing on the walls of the maze

---
+++ Assets/GreenMossGames/UltimateMaterialLibrary/Readme.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:534d37fc81c220ff71f8f9358d98ce8083388f5fad32ce73858a21cd325cd67f
+size 711
---
+++ Assets/PlayerMovementController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0a703ff00a955b2da71b45dff7519c74c0b0a197f2e2ac506673f52b9937e85
+size 1535
